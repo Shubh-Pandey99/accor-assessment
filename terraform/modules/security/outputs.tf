5aab412586c4b2ba573a0cdf9cf3b95f2ced5406
Shubh-Pandey99 <shubhpandet@gmail.com>
2026-05-07 02:10:00 +0530

    refactor: remove stale DynamoDB and SQS output definitions from security module

--- terraform/modules/security/outputs.tf
+++ terraform/modules/security/outputs.tf
@@ -17,11 +17,3 @@ output "fluent_bit_role_arn" {
 output "ecr_repository_url" {
   value = aws_ecr_repository.redemption.repository_url
 }
-
-output "dynamodb_table_name" {
-  value = aws_dynamodb_table.redemption_transactions.name
-}
-
-output "sqs_queue_url" {
-  value = aws_sqs_queue.redemption.url
-}
